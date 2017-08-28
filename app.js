ea583d804efe1079353b6077a17e3948ef1009c7
Katy Dillard <durhamdill@gmail.com>
2017-08-28 14:31:26 -0400

    add links to working and for hire robots

--- app.js
+++ app.js
@@ -15,6 +15,37 @@ app.set('view engine', 'mustache')
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({}).toArray(function (err, docs) {
+      res.render("index", {robots: docs})
+    })
+  })
+})
+
+app.get('/forhire', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({"job": null}).toArray(function (err, docs) {
+      res.render("index", {robots: docs})
+    })
+  })
+})
+
+app.get('/working', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    robots.find({"job": {$ne: null}}).toArray(function (err, docs) {
+      res.render("index", {robots: docs})
+    })
+  })
+})
+
+// app.get('/', function (req, res) {
+//   res.render('index');
+// });
+
 
 app.listen(3000, function () {
   console.log('Successfully started express application!')

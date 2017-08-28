ec7fb68d4a2e73466435fed6b3e002f7371e5185
Katy Dillard <durhamdill@gmail.com>
2017-08-28 16:37:31 -0400

    add individual profile pages

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
+
 // const dataSource = require('./dataSource.js');
 
 const mongodb = require('mongodb');
@@ -38,6 +39,38 @@ app.get('/working', function (req, res) {
     const robots = db.collection('robots');
     robots.find({"job": {$ne: null}}).toArray(function (err, docs) {
       res.render("index", {robots: docs})
+    })
+  })
+})
+
+app.get('/skills/:skills', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    let skills = req.params.skills;
+    robots.find({"skills": skills}).toArray(function (err, docs) {
+      res.render("index", {robots: docs, skills:skills})
+    })
+  })
+})
+
+app.get('/country/:enterCountry', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    let enterCountry = req.params.enterCountry;
+    robots.find({"address.country": enterCountry}).toArray(function (err, docs) {
+      res.render("index", {robots: docs, country: enterCountry})
+    })
+  })
+})
+
+app.get('/profile/:id', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robots = db.collection('robots');
+    let userid = parseInt(req.params.id);
+    console.log(userid);
+    console.log(typeof userid);
+    robots.find({id: userid}).toArray(function (err, docs) {
+      res.render("profile", {robots: docs})
     })
   })
 })
